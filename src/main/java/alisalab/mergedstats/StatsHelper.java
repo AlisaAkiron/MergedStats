6e0c11e8d1c690bb0c523c8a10c6ffae90983314
Liam Sho <conan_creeper@outlook.com>
2023-01-21 19:21:11 +0800

    refactor: simplify StatsHelper

--- src/main/java/alisalab/mergedstats/StatsHelper.java
+++ src/main/java/alisalab/mergedstats/StatsHelper.java
@@ -18,43 +18,18 @@ public final class StatsHelper {
     public static void updatePlayerData(ServerPlayerEntity p) {
         var handler = p.getStatHandler();
 
-        var minedStonesOverworld = getMinedStones(handler)
-                + getMinedCobblestones(handler)
-                + getMinedDeepslate(handler)
-                + getMinedCobbledDeepslate(handler)
-                + getMinedSandstone(handler)
-                + getMinedRedSandstone(handler)
-                + getMinedGranite(handler)
-                + getMinedDiorite(handler)
-                + getMinedAndesite(handler)
-                + getMinedCalcite(handler)
-                + getMinedTuff(handler);
-        var minedStonesNether = getMinedNetherrack(handler)
-                + getMinedCrimsonNylium(handler)
-                + getMinedWarpedNylium(handler)
-                + getMinedSoulSand(handler)
-                + getMinedSoulSoil(handler)
-                + getMinedBlackstone(handler)
-                + getMinedBasalt(handler)
-                + getMinedSmoothBasalt(handler)
-                + getMinedGlowstone(handler);
+        var minedStonesOverworld = getMinedStonesOverworld(handler);
+        var minedStonesNether = getMinedStonesNether(handler);
         var minedStonesTotal = minedStonesOverworld + minedStonesNether;
         var minedOresOverworld = getMinedOresOverworld(handler);
         var minedOresNether = getMinedOresNether(handler);
         var minedOresTotal = minedOresOverworld + minedOresNether;
         var playTimeMinutes = getPlayTime(handler) / 20 / 60;
-        var travelSelfGroundDistance = getWalkDistance(handler)
-                + getSprintDistance(handler);
-        var travelSelfWaterDistance = getSwimDistance(handler)
-                + getWalkOnWaterDistance(handler)
-                + getWalkUnderWaterDistance(handler);
+        var travelSelfGroundDistance = getSelfGroundDistance(handler);
+        var travelSelfWaterDistance = getSelfWaterDistance(handler);
         var travelSelfDistance = travelSelfGroundDistance + travelSelfWaterDistance;
-        var travelRideAnimalDistance = getHorseDistance(handler)
-                + getPigDistance(handler)
-                + getStriderDistance(handler);
-        var travelRideTransportationDistance = getElytraDistance(handler)
-                + getBoatDistance(handler)
-                + getMinecartDistance(handler);
+        var travelRideAnimalDistance = getRideAnimalDistance(handler);
+        var travelRideTransportationDistance = getRideTransportationDistance(handler);
         var travelRideDistance = travelRideAnimalDistance + travelRideTransportationDistance;
         var travelAllDistance = travelSelfDistance + travelRideDistance;
 
@@ -80,191 +55,32 @@ public final class StatsHelper {
         p.increaseStat(identifier, increase);
     }
 
-    /**
-     * Get minecraft.mined:minecraft.stone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedStones(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.STONE));
+    private static int getMinedStonesOverworld(StatHandler handler) {
+        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.STONE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.COBBLESTONE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DEEPSLATE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.COBBLED_DEEPSLATE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SANDSTONE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.RED_SANDSTONE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.GRANITE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DIORITE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.ANDESITE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.CALCITE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.TUFF));
     }
 
-    /**
-     * Get minecraft.mined:minecraft.cobblestone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedCobblestones(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.COBBLESTONE));
+    private static int getMinedStonesNether(StatHandler handler) {
+        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.NETHERRACK))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.CRIMSON_NYLIUM))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.WARPED_NYLIUM))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SOUL_SAND))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SOUL_SOIL))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.BLACKSTONE))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.BASALT))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SMOOTH_BASALT))
+                + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.GLOWSTONE));
     }
 
-    /**
-     * Get minecraft.mined:minecraft.deepslate stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedDeepslate(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DEEPSLATE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.cobbled_deepslate stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedCobbledDeepslate(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.COBBLED_DEEPSLATE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.sandstone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedSandstone(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SANDSTONE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.red_sandstone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedRedSandstone(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.RED_SANDSTONE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.granite stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedGranite(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.GRANITE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.diorite stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedDiorite(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DIORITE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.andesite stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedAndesite(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.ANDESITE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.calcite stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedCalcite(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.CALCITE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.tuff stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedTuff(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.TUFF));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.netherrack stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedNetherrack(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.NETHERRACK));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.crimson_nylium stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedCrimsonNylium(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.CRIMSON_NYLIUM));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.warped_nylium stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedWarpedNylium(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.WARPED_NYLIUM));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.soul_sand stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedSoulSand(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SOUL_SAND));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.soul_soil stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedSoulSoil(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SOUL_SOIL));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.blackstone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedBlackstone(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.BLACKSTONE));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.basalt stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedBasalt(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.BASALT));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.smooth_basalt stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedSmoothBasalt(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.SMOOTH_BASALT));
-    }
-
-    /**
-     * Get minecraft.mined:minecraft.glowstone stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
-    public static int getMinedGlowstone(StatHandler handler) {
-        return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.GLOWSTONE));
-    }
-
-    /**
-     * Get mined ores in the overworld stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
     private static int getMinedOresOverworld(StatHandler handler) {
         return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.COAL_ORE))
                 + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DEEPSLATE_COAL_ORE))
@@ -284,122 +100,36 @@ public final class StatsHelper {
                 + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.DEEPSLATE_DIAMOND_ORE));
     }
 
-    /**
-     * Get mined ores in the nether stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Blocks
-     */
     private static int getMinedOresNether(StatHandler handler) {
         return handler.getStat(Stats.MINED.getOrCreateStat(Blocks.NETHER_GOLD_ORE))
                 + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.NETHER_QUARTZ_ORE))
                 + handler.getStat(Stats.MINED.getOrCreateStat(Blocks.ANCIENT_DEBRIS));
     }
 
-    /**
-     * Get minecraft.custom:minecraft.play_time stats
-     * @param handler The stat handler of the player
-     * @return Stats count in Ticks (gt, 0.05 s)
-     */
-    public static int getPlayTime(StatHandler handler) {
+    private static int getPlayTime(StatHandler handler) {
         return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME));
     }
 
-    /**
-     * Get minecraft.custom:minecraft.walk_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getWalkDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_ONE_CM));
+    private static int getSelfGroundDistance(StatHandler handler) {
+        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.SPRINT_ONE_CM));
     }
 
-    /**
-     * Get minecraft.custom:minecraft.sprint_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getSprintDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.SPRINT_ONE_CM));
+    private static int getSelfWaterDistance(StatHandler handler) {
+        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.SWIM_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_ON_WATER_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_UNDER_WATER_ONE_CM));
     }
 
-    /**
-     * Get minecraft.custom:minecraft.swim_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getSwimDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.SWIM_ONE_CM));
+    private static int getRideAnimalDistance(StatHandler handler) {
+        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.HORSE_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PIG_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.STRIDER_ONE_CM));
     }
 
-    /**
-     * Get minecraft.custom:minecraft.walk_on_water_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getWalkOnWaterDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_ON_WATER_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.walk_under_water_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getWalkUnderWaterDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.WALK_UNDER_WATER_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.horse_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getHorseDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.HORSE_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.pig_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getPigDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PIG_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.strider_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getStriderDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.STRIDER_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.aviate_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getElytraDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.AVIATE_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.minecart_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getMinecartDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.MINECART_ONE_CM));
-    }
-
-    /**
-     * Get minecraft.custom:minecraft.boat_one_cm stats
-     * @param handler The stat handle of the player
-     * @return Stats count in centimeter
-     */
-    public static int getBoatDistance(StatHandler handler) {
-        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.BOAT_ONE_CM));
+    private static int getRideTransportationDistance(StatHandler handler) {
+        return handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.AVIATE_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.MINECART_ONE_CM))
+                + handler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.BOAT_ONE_CM));
     }
 }
